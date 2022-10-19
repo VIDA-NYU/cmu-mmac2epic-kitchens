7a37d86fbbcd61b7ebc4db3b18b1d86a92f058fd
Bea Steers <bea.steers@gmail.com>
2022-10-19 18:08:31 -0400

    support suffixes

--- convert.py
+++ convert.py
@@ -14,49 +14,56 @@ REMOVE_SUFFIXES = ['tl', 'tr', 'tm', 'bl', 'br', 'bm', 'l', 's', 'm']
 
 NON_NOUNS = ['1', '2', 'partailly', 'partially', 'patially']  # a bit manual but
 
-def main(ann_dir='Data', out_dir='output', target_recipe=None):
+def main(ann_dir='Data', out_dir='output', target_recipe=None, suffix='_retrieval_test', make_sentence_csv=True, **kw):
     # load src csv files
     dfs = []
     for f in tqdm.tqdm(glob.glob(os.path.join(ann_dir, 'Plans/*/*.txt'))):
-        df = convert_csv(f, target_recipe=target_recipe)
+        df = convert_csv(f, target_recipe=target_recipe, **kw)
         if df is None:
             continue
         dfs.append(df)
     df = pd.concat(dfs)
-
-    # get unique noun/verb
-    unique_verbs = df.verb.unique()
-    unique_nouns = np.unique(['']+[n for ns in df.all_nouns for n in ns])
-    print('verb classes:', len(unique_verbs), sorted(unique_verbs))
-    print('noun classes:', len(unique_nouns), sorted(unique_nouns))
-    
-    # convert noun/verb to index
-    verb_i_lookup = {v: i for i, v in enumerate(unique_verbs)}
-    noun_i_lookup = {n: i for i, n in enumerate(unique_nouns)}
-    df['verb_class']  = df.verb.apply(lambda v: verb_i_lookup[v])
-    df['noun_class']  = df.noun.apply(lambda n: noun_i_lookup[n])
-    df['all_noun_classes']  = df.all_nouns.apply(lambda ns: [noun_i_lookup[n] for n in ns])
+    df, _, _ = verb_noun_classes(df)
 
     # set column/row ordering
     df = df[COL_ORDER]
     df = df.set_index('narration_id').sort_index()
 
     # csv output prefix
-    suffix = f'_{target_recipe}' if target_recipe else ''
-    name = f'cmu_mmac{suffix}'
+    _suffix = f'_{target_recipe}' if target_recipe else ''
+    name = f'cmu_mmac{_suffix}{suffix}'
     print(name)
 
     # write out csvs/pkl files
     os.makedirs(out_dir, exist_ok=True)
-    df.to_csv(os.path.join(out_dir, f'{name}_retrieval_test.csv'))
-    df.to_pickle(os.path.join(out_dir, f'{name}_retrieval_test.pkl'))
+    df.to_csv(os.path.join(out_dir, f'{name}.csv'))
+    df.to_pickle(os.path.join(out_dir, f'{name}.pkl'))
 
-    sentence_df = df[['narration']].reset_index().groupby('narration').first().reset_index().set_index('narration_id')
-    sentence_df.to_csv(os.path.join(out_dir, f'{name}_retrieval_test_sentence.csv'))
-    sentence_df.to_pickle(os.path.join(out_dir, f'{name}_retrieval_test_sentence.pkl'))
+    if make_sentence_csv:
+        sentence_df = df[['narration']].reset_index().groupby('narration').first().reset_index().set_index('narration_id')
+        sentence_df.to_csv(os.path.join(out_dir, f'{name}_sentence.csv'))
+        sentence_df.to_pickle(os.path.join(out_dir, f'{name}_sentence.pkl'))
+    
     print(df)
 
-def convert_csv(csv_file, eaf_file=None, fps=30, target_recipe=None):
+
+def verb_noun_classes(df):
+    # get unique noun/verb
+    unique_verbs = df.verb.unique()
+    unique_nouns = np.unique(['']+[n for ns in df.all_nouns for n in ns])
+    print('verb classes:', len(unique_verbs), sorted(unique_verbs))
+    print('noun classes:', len(unique_nouns), sorted(unique_nouns))
+
+    # convert noun/verb to index
+    verb_i_lookup = {v: i for i, v in enumerate(unique_verbs)}
+    noun_i_lookup = {n: i for i, n in enumerate(unique_nouns)}
+    df['verb_class']  = df.verb.apply(lambda v: verb_i_lookup[v])
+    df['noun_class']  = df.noun.apply(lambda n: noun_i_lookup[n])
+    df['all_noun_classes']  = df.all_nouns.apply(lambda ns: [noun_i_lookup[n] for n in ns])
+    return df, unique_verbs, unique_nouns
+
+
+def convert_csv(csv_file, eaf_file=None, fps=30, target_recipe=None, norm=True):
     # read csv
     eaf_file = os.path.splitext(csv_file)[0] + '.eaf'
     df = pd.read_csv(csv_file, header=None)
@@ -97,6 +104,9 @@ def convert_csv(csv_file, eaf_file=None, fps=30, target_recipe=None):
     df['all_nouns'] = df.narration.apply(lambda s: [si for ss in s.split()[1:] for si in ss.split('&')])
     df['all_nouns'] = df.all_nouns.apply(lambda ns: [remove_suffix(n) for n in ns if n not in NON_NOUNS] or ['-'])
     df['noun'] = df.all_nouns.apply(lambda ns: ' '.join(ns[:1]))
+
+    if norm:
+        df['narration'] = df.apply(lambda r: ' '.join([r.verb]+r.all_nouns, axis=1)
 
     return df
 
